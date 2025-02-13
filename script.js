51e84248666fc5e58430e6f68e0277849c5966ae
Tanzina <shitultanzina@gmail.com>
2025-02-14 01:47:05 +0600

    All are up to date

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['MIT Student at SUST ','Web developer'],
+    strings:['MIT Student of SUST ','backend developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
